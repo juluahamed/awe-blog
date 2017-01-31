43a2049a6a75c9023c146a2530987fd0b55e7f77
Julu Ahamed <juluahamed@gmail.com>
2017-01-31 18:20:48 +0530

    Fix error statement for non-logged in users trying to comment/like

--- blog.py
+++ blog.py
@@ -312,19 +312,19 @@ class EditComment(BlogHandler):
             return
 
     def post(self, comb_id):
-            post_id = comb_id.split('+')[0]
-            comment_id = comb_id.split('+')[1]
-            key = get_post_key(int(post_id))
-            post = db.get(key)
-            c_key = db.Key.from_path('Comment', int(comment_id), parent = key)
-            comment = db.get(c_key)
-            if self.request.get('comment'):
-                new_comment = self.request.get('comment')
-                comment.comment = new_comment
-                comment.put()
-                self.redirect('/blog/%s' % post_id)
-            else:
-                self.render("editcomment.html", comment=comment, error="Your Comment was empty")
+        post_id = comb_id.split('+')[0]
+        comment_id = comb_id.split('+')[1]
+        key = get_post_key(int(post_id))
+        post = db.get(key)
+        c_key = db.Key.from_path('Comment', int(comment_id), parent = key)
+        comment = db.get(c_key)
+        if self.request.get('comment'):
+            new_comment = self.request.get('comment')
+            comment.comment = new_comment
+            comment.put()
+            self.redirect('/blog/%s' % post_id)
+        else:
+            self.render("editcomment.html", comment=comment, error="Your Comment was empty")
 
 
 class DeleteComment(BlogHandler):
